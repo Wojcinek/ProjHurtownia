4d48e53c08dd33d1661cc06121ef9debbad2be31
Wojciech Cygan <Wojciech.Cygan@microsoft.wsei.edu.pl>
2022-07-02 16:43:47 +0200

    walidacja email i telefon

--- projekt/klienci.xaml.cs
+++ projekt/klienci.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,15 +29,32 @@ namespace projekt
 
         private void Klienci_dodaj(object sender, RoutedEventArgs e)
         {
-             Klienci klienci = new Klienci()
+            string email = EmailText.Text;
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+            if (!match.Success)
+            {
+                MessageBox.Show("Błędny email");
+                return;
+            }
+
+            string telefon = TelefonText.Text;
+            Regex regex1 = new Regex(@"^[0-9]*$");
+            Match match1 = regex1.Match(telefon);
+            if (!match1.Success || TelefonText.Text.Length>9)
+            {
+                MessageBox.Show("Błędny numer");
+                return;
+            }
+
+            Klienci klienci = new Klienci()
             {
                 Imie = ImieText.Text,
                 Nazwisko = NazwiskoText.Text,
                 Adres = AdresText.Text,
                 Telefon = TelefonText.Text,
                 Email = EmailText.Text
-
-            };
+        };
 
             baza.Klienci.Add(klienci);
             try
@@ -80,5 +98,7 @@ namespace projekt
                 KlienciTabela.ItemsSource = baza.Klienci.ToList();
             }
         }
+
     }
 }
+
